fb5d56fa8481c08ee5162bd22221a4c42e926164
jerry-cheng5 <j.cheng55555@gmail.com>
2020-07-06 21:04:59 -0400

    Finished app instance and note component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,72 @@
+//Vue Components
+Vue.component('note', {
+    props: ['info'],
+    template: `
+        <div class="col-lg-3 col-md-6 note">
+            <div class="note-wrapper" data-toggle="modal" :data-target="targetId">
+                <h2>{{info.title}}</h2>
+                <p>{{info.contents}}</p>
+            </div>
+            <div :id="id" class="modal fade" tabindex="-1" role="dialog"
+                :aria-labelledby="ariaId" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <input v-model="info.title" placeholder="Title">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <textarea v-model="info.contents" placeholder="Take a note..."></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+    data () {
+        return {
+          id: null,
+          ariaId: null,
+          targetId: null,
+          lableId: null
+        }
+    }, 
+    mounted () {
+        this.id = 'n' + this._uid
+        this.ariaId = this.id + 'Label'
+        this.targetId = '#' + this.id
+    }
+})
+
+//Vue Instance
 var app = new Vue({
     el: '#app',
     data: {
         notes: [
             {
-                id: null,
-                title: "First Note",
+                title: "",
                 contents: "Testing content"
             },
             {
-                id: null,
                 title: "Second Note",
-                contents: "IWOEJFOIEWJOIFwjeio fjwie"
+                contents: ""
             },
             {
-                id: null,
-                title: "Third Note",
-                contents: "jwioe wjeoiwj eio fjoae fiwoe"
+                title: "",
+                contents: ""
             },
             {
-                id: null,
                 title: "Fourth Note",
                 contents: "jwioe wjeoiwj eio fjoae fiwoe"
             },
             {
-                id: null,
                 title: "Fifth Note",
                 contents: "jwioe wjeoiwj eio fjoae fiwoe"
             }
         ],
         currentNote: null
-    },
-    created () {
-        this.id = this._uid;
-        console.log(this._uid);
     },
     methods: {
         createNote(){
