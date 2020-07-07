7c444ecec973e42a2ede305b8c9c162df45e2c41
jerry-cheng5 <j.cheng55555@gmail.com>
2020-07-07 12:56:59 -0400

    randomly generated ids

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,68 @@
+//Functions
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
 //Vue Components
+Vue.component('notes', {
+    template: `
+        <div class="container">
+            <h1>Notes</h1>
+            <button @click="createNote" class="btn btn-primary">New Note</button>
+            <div class="notes">
+                <p v-if="!notes.length">No notes saved.</p>
+                <div v-if="notes.length" class="row">
+                    <note v-for="note in notes" :key="note.id" :info="note"></note>
+                </div>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            notes: [
+                {
+                    title: "",
+                    contents: "Testing content",
+                    id: 'n' + uuidv4()
+                },
+                {
+                    title: "Second Note",
+                    contents: "",
+                    id: 'n' + uuidv4()
+                },
+                {
+                    title: "",
+                    contents: "",
+                    id: 'n' + uuidv4()
+                },
+                {
+                    title: "Fourth Note",
+                    contents: "jwioe wjeoiwj eio fjoae fiwoe",
+                    id: 'n' + uuidv4()
+                },
+                {
+                    title: "Fifth Note",
+                    contents: "jwioe wjeoiwj eio fjoae fiwoe",
+                    id: 'n' + uuidv4()
+                }
+            ],
+            currentNote: null
+        }
+    },
+    methods: {
+        createNote(){
+            const newNote = {title: "", contents: "", id: 'n' + uuidv4()};
+            this.notes.push(newNote);
+        },
+        deleteNote(note){
+            this.notes = this.notes.filter(function(e) { return e !== note })
+        }
+    }
+})
+
+
 Vue.component('note', {
     props: ['info'],
     template: `
@@ -8,7 +72,7 @@ Vue.component('note', {
                 <h2 v-if="info.title">{{info.title}}</h2>
                 <p v-if="info.contents" v-bind:style="[(!info.title) ? {'padding-top': '12px'} : {'padding-top': '0px'}]">{{info.contents}}</p>
             </div>
-            <div :id="id" class="modal fade" tabindex="-1" role="dialog"
+            <div :id="info.id" class="modal fade" tabindex="-1" role="dialog"
                 :aria-labelledby="ariaId" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
@@ -28,58 +92,19 @@ Vue.component('note', {
     `,
     data () {
         return {
-          id: null,
           ariaId: null,
-          targetId: null,
-          lableId: null
+          targetId: null
         }
     }, 
     mounted () {
-        this.id = 'n' + this._uid
-        this.ariaId = this.id + 'Label'
-        this.targetId = '#' + this.id
+        this.ariaId = this.info.id + 'Label'
+        this.targetId = '#' + this.info.id
     }
 })
 
 //Vue Instance
 var app = new Vue({
-    el: '#app',
-    data: {
-        notes: [
-            {
-                title: "",
-                contents: "Testing content"
-            },
-            {
-                title: "Second Note",
-                contents: ""
-            },
-            {
-                title: "",
-                contents: ""
-            },
-            {
-                title: "Fourth Note",
-                contents: "jwioe wjeoiwj eio fjoae fiwoe"
-            },
-            {
-                title: "Fifth Note",
-                contents: "jwioe wjeoiwj eio fjoae fiwoe"
-            }
-        ],
-        currentNote: null
-    },
-    methods: {
-        createNote(){
-            const newNote = {title: "", contents: ""};
-            this.notes.push(newNote);
-        }
-    },
-    created(){
-        for (var i=0; i<this.notes.length; i++){
-            if (notes[i].title === "" && notes[i].contents === ""){
-                notes.splice(i, 1);
-            }
-        }
-    }
+    el: '#app'
 })
+
+
